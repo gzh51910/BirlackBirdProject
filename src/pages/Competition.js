ba9b879aff03f6fd6e2da94ff7c8a42458657155
huangxinhuangxinhuangxinhuangxinhu <1269383158@qq.com>
2019-12-02 21:24:05 +0800

    添加跳转

--- src/pages/Competition.js
+++ src/pages/Competition.js
@@ -24,7 +24,6 @@ class Competition extends Component {
         this.setState({
             commedList
         })
-
     }
     render() {
         let { commedList } = this.state
--- src/pages/CompetitionDetails.js
+++ src/pages/CompetitionDetails.js
@@ -29,8 +29,10 @@ class CompetitionDetails extends Component {
         this.setState({
             commedList
         })
-
     }
+    goto=(id)=>{
+        this.props.history.push(`/Enlist`)
+        }
     render() {
         let { commedList } = this.state
 
@@ -53,7 +55,7 @@ class CompetitionDetails extends Component {
                                     <p className="CompetitionDetails_title">{item.activityTitle}</p>
                                     <p>线路	<span>{item.circuit}</span></p>
                                     <p>报名截止时间	<span>{apply_endTime}</span></p>
-                                    <p>比赛时间	<span>{item.startTime} 至 {endTime}</span></p>
+                                    <p>比赛时间	<span>{endTime} 至 {item.startTime}</span></p>
                                     <p>地点	<span>{item.assemblyAddress}</span></p>
                                     <img src={item.imageLink} />
                                 </div>
@@ -87,11 +89,15 @@ class CompetitionDetails extends Component {
                                 </Tabs>
                             </div>
                             <div className="CompetitionDetails_footer">
-
+                            </div>
+                            <div className="CompetitionDetails_main_bottom">
+                                      <h3>观看直播</h3>
+                                      <h4 onClick={this.goto.bind(this,item.activtyId)}>立即报名</h4>
                             </div>
                         </div>
                     })
                 }
+                  
             </div>
         )
     }
